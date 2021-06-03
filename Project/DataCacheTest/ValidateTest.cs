a98138c00b9d9535b6359b9a7aa58360cbbe1677
DraganaLukic <draganal473@gmail.com>
2021-06-03 05:11:02 +0200

    testup

--- Project/DataCacheTest/ValidateTest.cs
+++ Project/DataCacheTest/ValidateTest.cs
@@ -11,14 +11,36 @@ namespace DataCacheTest
     class ValidateTest
     {
         [Test]
-        public void CheckValidateEntry(string startDate, string endDate, string geoArea)
+        public void CheckValidateEntry()
         {
-            string goodStartDate = "3/21/2020";
+            string goodStartDate = "3/21/2018";
             string goodEndDate = "4/1/2020";
             string goodArea = "SRB";
 
+            string baadStartDateYear = "5/5/2020";
+            string badEndDateYear = "7/5/2018";
+
+            string baadStartDateMonth = "7/5/2020";
+            string badEndDateMonth = "5/5/2020";
+
+            string baadStartDateDay = "5/7/2020";
+            string badEndDateDay = "5/5/2020";
+
             DataCache.Validate validate = new DataCache.Validate();
-            Assert.AreEqual("", validate.ValidateEntry(goodStartDate, goodEndDate, geoArea));
+            Assert.AreEqual("", validate.ValidateEntry(goodStartDate, goodEndDate, goodArea));
+
+            Assert.AreEqual("All fields must be filled in correctly", validate.ValidateEntry("", "", ""));
+            Assert.AreEqual("All fields must be filled in correctly", validate.ValidateEntry(goodStartDate, goodEndDate, ""));
+            Assert.AreEqual("All fields must be filled in correctly", validate.ValidateEntry("", goodEndDate, goodArea));
+            Assert.AreEqual("All fields must be filled in correctly", validate.ValidateEntry(goodStartDate, "", goodArea));
+
+            Assert.AreEqual("Starting date must be lower then ending date", validate.ValidateEntry(baadStartDateYear, badEndDateYear, goodArea));
+            Assert.AreEqual("Starting date must be lower then ending date", validate.ValidateEntry(baadStartDateDay, badEndDateDay, goodArea));
+            Assert.AreEqual("Starting date must be lower then ending date", validate.ValidateEntry(baadStartDateMonth, badEndDateMonth, goodArea));
+            
+
+
+
 
         }
     }
